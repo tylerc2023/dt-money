ceffb593ea47b4535687d9011268a5398df522a1
tylerc2023 <tylerc2023@gmail.com>
2022-04-25 11:38:21 -0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
+import styled from 'styled-components';
 
+const Title = styled.h1`
+   color: #8257e6;
+
+`
 
 export function App() {
   return (
     <div className="App">
-     <h1 className="title">Hello World!</h1>
+     <Title>Hello World!</Title>
     </div>
   );
 }
