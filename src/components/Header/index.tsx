bf1ba6be4d25dac36587866f1043044c8ff5c92d
tylerc2023 <tylerc2023@gmail.com>
2022-04-27 12:33:43 -0700

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import logoImg from '../../assets/logo.svg';
-import { Container } from './styles';
-
+import { Container, Content } from './styles';
 
 export function Header(){
     return(
         <Container>
+          <Content>
           <img src={logoImg} alt="dt money" />
           <button type="button">
               Nova transação
           </button>
+          </Content>
         </Container>
     );
 }
