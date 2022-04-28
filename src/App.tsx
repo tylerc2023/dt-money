376f32c9339dcaa61da481f52ae3fbbc2acb9c58
tylerc2023 <tylerc2023@gmail.com>
2022-04-28 06:28:41 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import {GlobalStyle} from './styles/global';
 
@@ -5,8 +6,11 @@ export function App() {
   return (
     <>
       <Header/>
+      <Dashboard/>
      <GlobalStyle/>
     </>
   );
 }
+
+
 
