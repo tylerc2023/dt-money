931d94402e2668cf07862c5a102230291147f761
tylerc2023 <tylerc2023@gmail.com>
2022-04-28 07:17:39 -0700

    Update styles.ts

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+    display: grid; //porque são três ítens do mesmo tamanho e um do lado do outro(o flex tbm serve nessas situações).
+    grid-template-columns: repeat(3, 1fr); //três colunasde tamanhos iguais. (tbm funciona o 1fr 1fr 1fr).
+    gap: 2rem; //espaçamento entre cada um dos ítens no grid.
+    margin-top: -10rem; //jogando o summary mais para cima.
+
+    //estilizando cada ítem
+    div {
+        background: var(--shape);
+        padding: 1.5rem 2rem;
+        border-radius: 0.25rem;
+        color: var(--text-title);
+
+        header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        strong {
+            margin-top: 1rem;
+            font-size: 2rem;
+            font-weight: 500;
+            line-height: 3rem;
+        }
+    }
 `;
