f846d2ab50d93409f7ffc018eb6f1b2d3707fe2b
tylerc2023 <tylerc2023@gmail.com>
2022-05-02 04:29:00 -0700

    Update styles.ts

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -1,1 +1,22 @@
-//
+import styled from 'styled-components';
+
+export const Container = styled.div`
+     margin-top: 4rem;
+
+     table {
+         width: 100%; //para a table não ocupar toda a largura
+         border-spacing: 0 0.5rem;
+     }
+
+     th {
+         color: var(--text-body);
+         font-weight: 400;
+         padding: 1rem 2rem;
+         text-align: left;
+         line-height: 1.5rem;
+     }
+
+     td {
+         
+     }
+`;
