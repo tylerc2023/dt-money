e5381f5b8f6d2964db54a5870192e91263c2677d
tylerc2023 <tylerc2023@gmail.com>
2022-05-02 13:37:36 -0700

    Update index.tsx

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -15,22 +15,17 @@ export function TransactionsTable() {
                 <tbody>
                     <tr>
                         <td>Desenvolvimento de site</td>
-                        <td>R$12.000</td>  
+                        <td className="deposito">R$12.000</td>  
                         <td>Desenvolvimento</td> 
                         <td>20/02/2021</td>
                     </tr>
                     <tr>
-                        <td>Desenvolvimento de site</td>
-                        <td>R$12.000</td>  
-                        <td>Desenvolvimento</td> 
+                        <td>Aluguel</td>
+                        <td className="withdraw">R$1.000</td>  
+                        <td>Casa</td> 
                         <td>20/02/2021</td>
                     </tr>
-                    <tr>
-                        <td>Desenvolvimento de site</td>
-                        <td>R$12.000</td>  
-                        <td>Desenvolvimento</td> 
-                        <td>20/02/2021</td>
-                    </tr>
+ 
                 </tbody>
 
             </table>
