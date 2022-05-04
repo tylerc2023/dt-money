6717cc6f89cd17491169cdb9700ebdc03af427f6
tylerc2023 <tylerc2023@gmail.com>
2022-05-04 07:00:07 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,20 @@ import {App} from './App';
 createServer({
   routes() {
     this.namespace = 'api';
+
+    //para cada rota:
+    this.get('/transacitons', () => {
+      return [ //posso retornar o que eu quiser
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        } //alguns campos de valor retornado da api
+      ]
+    })
   }
 })
 
