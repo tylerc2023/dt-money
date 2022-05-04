b71fc8ef0053056514ab7c395574d6fe5425f13b
tylerc2023 <tylerc2023@gmail.com>
2022-05-04 08:06:21 -0700

    Update app.ts

--- src/services/app.ts
+++ src/services/app.ts
@@ -1,1 +1,8 @@
 //serviços de dados
+
+import axios from 'axios';
+
+export const api = axios.create({
+    //criar instâncias p/ setar informações que são padrão para todas as requisições
+    baseURL: 'http://localhost:3000/api',  //endereço que se repete.
+})
