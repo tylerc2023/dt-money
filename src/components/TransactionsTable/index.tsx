fd395e71b2c41fd84e3bb942e4b44210a778d39d
tylerc2023 <tylerc2023@gmail.com>
2022-05-04 08:04:57 -0700

    Update index.tsx

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -4,7 +4,7 @@ import { Container } from "./styles";
 export function TransactionsTable() {
 
       useEffect(() => {
-        fetch('http://localhost:3000/api/transactions')
+        fetch('/transactions')
         .then(response => response.json())
         .then(data => console.log(data))
     },[]);
