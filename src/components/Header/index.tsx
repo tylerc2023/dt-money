3752c53f0f0409375286978eab0ab7a68538afde
tylerc2023 <tylerc2023@gmail.com>
2022-05-12 15:36:52 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,29 @@
-import logoImg from '../../assets/logo.svg';
-import { Container, Content } from './styles';
+import logoIMG from "../../assets/logo.svg"; //importando img de assets
+import {Container, Content} from './styles';
+import {useState} from 'react';
 
-export function Header(){
+
+export function Header() {
+
+    //criando o modal
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        //sempre que uma função começa com handle é pq o user vai clicar em algo
+        setIsNewTransactionModalOpen(false);
+    }
+
     return(
         <Container>
-          <Content>
-          <img src={logoImg} alt="dt money" />
-          <button type="button">
-              Nova transação
-          </button>
-          </Content>
+            <Content>
+            <img src={logoIMG} alt="dt money" />
+            <button type="button" onClick={handleOpenNewTransactionModal}>
+                Nova transação
+            </button>
+            </Content>
         </Container>
     );
 }
