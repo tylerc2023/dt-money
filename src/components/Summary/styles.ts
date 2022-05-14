8eabd6d252379eef7be12759e8a4a0a7463169c3
tylerc2023 <tylerc2023@gmail.com>
2022-05-14 08:39:58 -0300

    Update styles.ts

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -2,11 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: grid; //porque são três ítens do mesmo tamanho e um do lado do outro(o flex tbm serve nessas situações).
-    grid-template-columns: repeat(3, 1fr); //três colunasde tamanhos iguais. (tbm funciona o 1fr 1fr 1fr).
+    grid-template-columns: repeat(3, 1fr); 
     gap: 2rem; //espaçamento entre cada um dos ítens no grid.
     margin-top: -10rem; //jogando o summary mais para cima.
 
-    //estilizando cada ítem
     div {
         background: var(--shape);
         padding: 1.5rem 2rem;
@@ -20,7 +19,7 @@ export const Container = styled.div`
         }
 
         strong {
-            display: block; //para tira o padrão do strong que é o inline aí o margin-top não funcionaria. Ele faz o strong se comportar como se fosse uma div.
+            display: block; //tira o padrão do strong que é o inline aí o margin-top não funcionaria. Ele faz o strong se comportar como se fosse uma div.
             margin-top: 1rem;
             font-size: 2rem;
             font-weight: 500;
