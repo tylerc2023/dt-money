d002bd30e9e62081d16752f4d8294d65b5756b28
tylerc2023 <tylerc2023@gmail.com>
2022-05-14 08:40:41 -0300

    Update index.tsx

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,4 @@
 import { Container } from './styles';
-
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
