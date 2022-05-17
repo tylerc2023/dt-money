85cdac3427293759e7d9f43cf1d4cdc5efa0d8e8
tylerc2023 <tylerc2023@gmail.com>
2022-05-17 10:55:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
-import {GlobalStyle} from './styles/global';
-import Modal from  'react-modal';
+import {GlobalStyle} from "./styles/global";
+import {Header} from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal';
 
 
 export function App() {
+
+  //criando o modal
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      //sempre que uma função começa com handle é pq o usuário vai clicar em algo
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <Header/>
       <Dashboard/>
-       <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
                 <h2>
                     Cadastrar Transação
                 </h2>
       </Modal> 
-     <GlobalStyle/>
+      <GlobalStyle/>
     </>
   );
 }
-
 
 
