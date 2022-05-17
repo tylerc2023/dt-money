018efbee1ef91e79ed6a31a939a01267ed89372b
tylerc2023 <tylerc2023@gmail.com>
2022-05-17 10:56:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {GlobalStyle} from "./styles/global";
 import {Header} from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import Modal from 'react-modal';
+import {useState} from 'react';
 
 
 export function App() {
