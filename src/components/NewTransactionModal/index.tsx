f72e36db7d493b29de9efe6fc5398659ccc88648
tylerc2023 <tylerc2023@gmail.com>
2022-05-18 09:20:05 -0300

    Update index.tsx

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,1 +1,11 @@
 
+export function NewTransactionModal() {
+    return(
+        <Modal isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}>
+                  <h2>
+                      Cadastrar Transação
+                  </h2>
+        </Modal> 
+    );
+}
