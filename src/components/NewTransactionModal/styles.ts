f3a413ffdac75aba15db5d367c4bfd59cde1d55e
tylerc2023 <tylerc2023@gmail.com>
2022-05-19 09:33:05 -0300

    Update styles.ts

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,93 +1,58 @@
-import styled from 'styled-components';
-import {darken} from 'polished';
-// a função darken escurece uma cor
+import Modal from 'react-modal';
+import { Container, TransactionTypeContainer, RadioBox} from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { useState } from 'react';
 
-export const Container = styled.form`
-    h2 {
-        color: var(--text-title);
-        font-size: 1.5rem;
-        margin-bottom: 2rem;
-    }
 
-    input {
-        width: 100%;
-        padding: 0 1.5rem;
-        height: 4rem;
-        border-radius: 0.25rem;
 
-        border: 1px solid #d7d7d7;
-        background: #e7e9ee;
-
-        font-weight: 400;
-        font-size: 1rem;
-
-        &::placeholder {
-            color: var(--text-body);
-        }
-
-        & + input {
-            margin-top: 1rem;
-        }
-       
-    }
-
-    button[type="submit"] {
-        width: 100%;
-        padding: 0 1.5rem;
-        height: 4rem;
-        background: var(--green);
-        color: #fff;
-        border-radius: 0.25rem;
-        border: 0;
-        font-size: 1rem;
-        margin-top: 1.5rem;
-        font-weight: 600;
-
-        transition: filter 0.2s;
-
-        &:hover {
-            filter: brightness(0.9);
-        }
-    }
-`;
-
-export const TransactionTypeContainer = styled.div`
-    margin: 1rem 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr; //duas colunas
-    gap: 0.5rem; //espaçamento entre cada botão
-`;
-
-interface RadioBoxProps { //para não dar mais erro na isActive
-    isActive: boolean;
+interface NewTransactionModaProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
 }
 
-export const RadioBox  = styled.button<RadioBoxProps>` 
-     height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
-        
-        background: transparent;
+export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionModaProps) {
+    const [type, setType]  = useState('deposit'); //criando estado para armazenar o input
 
-        display: flex;
-        align-items: center;
-        justify-content: center;
+    
 
-        transition: border-color 0.2s;
+    return(
+        <Modal isOpen={isOpen} 
+        onRequestClose={onRequestClose} 
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content">
+        <button type="button"
+         onClick={onRequestClose} 
+         className="react-modal-close">
+            <img src={closeImg} alt="Fechar modal" />
+        </button>
+            <Container>
+              <h2>Cadastrar Transação</h2>
+              <input placeholder="Título" />
+              <input type="number" placeholder="Valor" />
 
-        &:hover { //nesse caso não conseguimos usar o filter pq ele escurece por completo e não só as bordas de um botão
-            border-color: ${darken(0.1, '#d7d7d7')};
-        }
+              <TransactionTypeContainer>
+                 <RadioBox type="button"
+                 onClick={() => {setType('deposit');}}
+                 isActive={type === 'deposit'}
+                 >
+                    <img src={incomeImg} alt="Entrada" />
+                    <span>Entrada</span>
+                 </RadioBox>
 
-        img {
-            width: 20px;
-            height: 20px;
-        }
+                 <RadioBox type="button"
+                 onClick={() => {setType('withdraw');}}
+                 isActive={type === 'withdraw'}
+                 >
+                    <img src={outcomeImg} alt="Saída" />
+                    <span>Saída</span>
+                 </RadioBox>
+              </TransactionTypeContainer>
 
-        span {
-            display: inline-block;
-            margin-left: 1rem;
-            font-size: 1rem;
-            color: var(--text-title);
-        }
-`;
+              <input placeholder="Categoria" />
+              <button type="submit">Cadastrar</button>
+            </Container>
+        </Modal> 
+    );
+}
