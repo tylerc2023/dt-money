5194ccb20774521fc02caa63f7af8475b24eb3b1
tylerc2023 <tylerc2023@gmail.com>
2022-05-27 10:31:48 -0300

    Update TransactionsContext.tsx

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,5 +1,23 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { api } from './services/api';
 
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
 //para qualquer componente ter acesso ao contexto precisamos por em volta um provider
 export const TransactionsContext = createContext([]); //forma mais simples de criar um contexto no react
+
+export function TransactionsProvider() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]); //como são várias transações usaremos sempre um array vazio
+
+    useEffect(() => {
+      api.get('transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, []); 
+}
