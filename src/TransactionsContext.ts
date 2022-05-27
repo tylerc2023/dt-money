7ec17dc3804dc0a695994cf16943df0cd7e071f6
tylerc2023 <tylerc2023@gmail.com>
2022-05-27 09:48:15 -0300

    Update TransactionsContext.ts

--- src/TransactionsContext.ts
+++ src/TransactionsContext.ts
@@ -1,4 +1,5 @@
 import { createContext } from 'react';
 
 
+//para qualquer componente ter acesso ao contexto precisamos por em volta um provider
 export const TransactionsContext = createContext([]); //forma mais simples de criar um contexto no react
