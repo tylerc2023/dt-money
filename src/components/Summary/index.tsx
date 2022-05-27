6d13831808d2a412bdbed4c7a0b364cd220cac3e
tylerc2023 <tylerc2023@gmail.com>
2022-05-27 09:59:33 -0300

    Update index.tsx

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,44 @@
-import { Container } from './styles';
-import incomeImg from '../../assets/income.svg';
-import outcomeImg from '../../assets/outcome.svg';
-import totalImg from '../../assets/total.svg';
+import { Container } from "./styles";
+
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+import {TransactionsContext} from './TransactionsContext';
 
 export function Summary() {
+    //obtendo os valores do contexto (há duas formas, usando handle props ou )
+    
+
     return(
         <Container>
+            <TransactionsContext.Consumer> 
+               {(data) => {
+                  console.log(data);
+               }}
+
+               return <p>olá</p>
+            </TransactionsContext.Consumer>
             <div>
                 <header>
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>R$ 1000,00</strong>
             </div>
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong>- R$500,00</strong>
+                <strong>- R$ 500,00</strong>
             </div>
-            <div className="highlight-background">
+            <div className="highliht-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$500,00</strong>
+                <strong>R$ 500,00</strong>
             </div>
         </Container>
     );
-} 
+}
