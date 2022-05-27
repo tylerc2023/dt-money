e16dc5cc6a38d05b6d5e496c882a8a15eea5f0de
tylerc2023 <tylerc2023@gmail.com>
2022-05-27 10:07:53 -0300

    Update index.tsx

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,6 +3,7 @@ import React, {useContext} from 'react';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import {TransactionsContext} from '../../TransactionsContext'
 
 
 export function Summary() {
