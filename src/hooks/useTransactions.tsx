8490a5e26167e8d6ccef16ef80a11b088b8f8389
tylerc2023 <tylerc2023@gmail.com>
2022-06-09 17:41:28 -0300

    Update useTransactions.tsx

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState, ReactNode, useContext } from 'react';
-import { api } from './services/api';
+import { api } from '../services/api';
 
 
 interface Transaction {
